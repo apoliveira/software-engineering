4edc166798035d9789d03581a06595a9b48a1f8b
Andre Oliveira <oliveira.andrepinto@gmail.com>
2015-04-07 10:43:31 -0400

    Add routes: index, login, signup

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var index = require('./routes/index');
+var login = require('./routes/login');
+var signup = require('./routes/sigup');
 
 var app = express();
 
@@ -22,8 +23,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', index);
+app.use('/login', login);
+app.use('/signup', signup);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/signup.js
+++ routes/signup.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Project Blackhawk' });
+  res.render('signup', { title: 'Sign Up' });
 });
 
 module.exports = router;
