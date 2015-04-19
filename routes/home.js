0956f951878be9d63b6134af79ebb533e3eb7bfd
Tyler White <considerateilliterate@gmail.com>
2015-04-19 15:42:07 -0400

    Added login verification and redirects to page

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Project Blackhawk' });
+  if( req.isAuthenticated() ) {
+    res.render('home', { title: 'Project Blackhawk' });
+  } else {
+    res.redirect("/login");
+  }
 });
+
 
 module.exports = router;
