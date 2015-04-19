6d473418d5da3fbed86c0852e1c7e2583e0e0c22
Tyler White <considerateilliterate@gmail.com>
2015-04-19 16:23:47 -0400

    Added empty pdf array to be passed to through to page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Project Blackhawk' });
+  res.render('index', { title: 'Project Blackhawk', pdfs: [] });
 });
 
 module.exports = router;
