e6723ae36cbe0d1653ca22344b28b442a5974e82
Tyler White <considerateilliterate@gmail.com>
2015-04-19 20:18:25 -0400

    Fixed passed in user object reference

--- routes/settings.js
+++ routes/settings.js
@@ -4,10 +4,10 @@ var router = express.Router();
 /* GET settings page. */
 router.get('/', function(req, res, next) {
     if(req.isAuthenticated() ) {
-      res.render('settings', { title: 'Settings', user: user});
+      res.render('settings', { title: 'Settings', user: req.user });
     } else {
       res.redirect("/signup");
-    {
+    }
 });
 
 module.exports = router;
